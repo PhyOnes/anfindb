a8b22623bb1b318541955beb5e2dee14071474e6
PhyOnes <phionpons@gmail.com>
2023-12-15 13:13:34 +0700

    Done

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="ztyle.css" />
+    <title>Document</title>
+</head>
+
+<body>
+    <div style="text-align: center">
+        <h1>Click this button :</h1>
+        <a href="viewTask.php"><button style="padding: 20px 40px; font-size: 20px; cursor: pointer;">CRUD</button></a>
+    </div>
+</body>
+
+</html>
